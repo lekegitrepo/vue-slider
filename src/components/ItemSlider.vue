d8351484a467a3eb3e4c274e81569f907cb60eb1
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-04-16 19:54:59 +0100

    Add slider contents

--- src/components/ItemSlider.vue
+++ src/components/ItemSlider.vue
@@ -1,5 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <v-app id="inspire">
+      <v-carousel v-model="model">
+        <v-carousel-item v-for="(color, i) in colors" :key="color">
+          <v-sheet :color="color" height="100%" tile>
+            <v-row class="fill-height" align="center" justify="center">
+              <div class="display-3">Slide {{ i + 1 }}</div>
+            </v-row>
+          </v-sheet>
+        </v-carousel-item>
+      </v-carousel>
+    </v-app>
+  </div>
 </template>
 
 <script>
