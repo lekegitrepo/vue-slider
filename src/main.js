04cfbbb74286cb7462e93462c013916472aa099f
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-04-16 20:17:39 +0100

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 import Vue from "vue";
-import Vuetify from "vuetify";
 import App from "./App.vue";
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
+
 new Vue({
-  vuetify: new Vuetify(),
-  data: () => ({
-    model: 0,
-    colors: ["primary", "secondary", "yellow darken-2", "red", "orange"],
-  }),
-  render: (h) => h(App),
+  vuetify,
+  render: (h) => h(App)
 }).$mount("#app");
