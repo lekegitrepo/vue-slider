7a6d3fddf8474566db0704f79b6debcdb26ff2e1
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-04-17 09:41:38 +0100

    Add transition

--- src/components/ItemSlider.vue
+++ src/components/ItemSlider.vue
@@ -6,8 +6,8 @@
           v-for="(item, i) in items"
           :key="i"
           :src="item.src"
-          reverse-transition="fade"
-          transition="fade"
+          reverse-transition="fade-transition"
+          transition="fade-transition"
         ></v-carousel-item>
       </v-carousel>
     </v-app>
