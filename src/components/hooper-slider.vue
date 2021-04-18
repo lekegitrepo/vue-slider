076010e7b902b640b75e36acef05cc6f7be3f2ce
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-04-18 18:24:21 +0100

    Install and create hooper-slider component

---
+++ src/components/hooper-slider.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <button @click.prevent="slidePrev">prev</button>
+    <input v-model="carouselData" type="number" min="0" max="5" />
+    <button @click.prevent="slideNext">next</button>
+
+    <hooper ref="carousel" @slide="updateCarousel">
+      <slide>
+        slide 1
+      </slide>
+      <slide>
+        slide 2
+      </slide>
+      <slide>
+        slide 3
+      </slide>
+      <slide>
+        slide 4
+      </slide>
+      <slide>
+        slide 5
+      </slide>
+      <slide>
+        slide 6
+      </slide>
+    </hooper>
+  </div>
+</template>
+
+<script>
+import { Hooper, Slide } from "hooper";
+import "hooper/dist/hooper.css";
+
+export default {
+  components: {
+    Hooper,
+    Slide,
+  },
+  data() {
+    return {
+      carouselData: 0,
+    };
+  },
+  watch: {
+    carouselData() {
+      this.$refs.carousel.slideTo(this.carouselData);
+    },
+  },
+  methods: {
+    slidePrev() {
+      this.$refs.carousel.slidePrev();
+    },
+    slideNext() {
+      this.$refs.carousel.slideNext();
+    },
+    updateCarousel(payload) {
+      this.myCarouselData = payload.currentSlide;
+    },
+  },
+};
+</script>
